daede14c8d8454592a2d2f2b4cf68f48519c6610
Anthony Perez <stockcart21@yahoo.com>
2017-12-05 15:58:22 -0800

    Update views.py

--- community/views.py
+++ community/views.py
@@ -2,11 +2,8 @@ from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-
 class PostList(ListView):
     model = Post
     paginate_by = 25
-
-
 class PostDetail(DetailView):
     model = Post
