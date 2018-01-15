b88c6c8b160a55fc922de2360a922e2f42993ee9
Anthony Perez <stockcart21@yahoo.com>
2018-01-14 22:29:54 -0800

    Update admin.py

--- codesamples/admin.py
+++ codesamples/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 
 from .models import CodeSample
 from cms.admin import ContentManageableModelAdmin
-
-
 admin.site.register(CodeSample, ContentManageableModelAdmin)
